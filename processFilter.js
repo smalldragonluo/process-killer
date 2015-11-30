d251b6529d87a12fee0b1c587990247e1d1cd57f
龙喜 <xiaolong.lxl@alibaba-inc.com>
2015-11-30 13:29:30 +0800

    update

--- processFilter.js
+++ processFilter.js
@@ -2,93 +2,129 @@
  * smalldragonluo created at 2015-11-13 11:19:23
  */
 
-var fs = require('fs');
 var path = require('path');
 var xtpl = require('xtpl');
 var exec = require('child_process').exec;
 var iconv = require('iconv-lite');
 var targetEncoding = 'utf8';
 var originEncoding = 'binary';
-var iconNames = require('./iconNames');
 
 module.exports = function(query) {
   // 渲染到 Alfred 的列表
-  var items = [];
+  var params = query.match(/\s*(-i\s+(\d+)\s*)|(.+)/);
 
-  // 查找进程信息，-c command 栏只包含可执行名称，取其作为 title (Change the command column output to just contain the executable name, rather than the full command line.)
-  exec('ps -Ac | grep -i ' + query + ' | grep -v grep', {
-    encoding: originEncoding,
-    maxBuffer: 1024 * 1024
-  }, function(error, stdout, stderr) {
-    // 如果出现错误，可能是 ps 未找到相关信息 导致 grep 出错
-    if (error) {
-      console.log('未找到相关进程');
-      return;
-    }
+  if (!params) {
+    return;
+  }
 
-    // 再取一次，获得完整的执行路径，包含传入参数
-    exec('ps -A | grep -i ' + query + ' | grep -v grep', {
+  // 将空格转义
+  if (params[3]) {
+    params[3] = params[3].replace(/\s/, '\\ ');
+  }
+
+  if (params[2]) {
+    exec('lsof -i4:' + params[2] + (params[3] ? ' | grep ' + params[3] : ''), {
       encoding: originEncoding,
       maxBuffer: 1024 * 1024
-    }, function(error2, stdout2, stderr2) {
-      if (error) {
-        console.error(error.stack);
-        return;
-      }
+    }, function(error, stdout, stderr) {
+      var infoRows = iconv.decode(new Buffer(stdout, originEncoding), targetEncoding).split(/\n/);
+      var pids = [];
 
-      var rows = iconv.decode(new Buffer(stdout, originEncoding), targetEncoding).split(/\n/);
-      var rows2 = iconv.decode(new Buffer(stdout2, originEncoding), targetEncoding).split(/\n/);
+      for (var i = 0; i < infoRows.length; i++) {
+        var params = infoRows[i].match(/^[^\d]+(\d+)/);
 
-      // 进程 map，取完整路径与参数用
-      var processMap = {};
-
-      for (var i = 0; i < rows2.length; i++) {
-        if (rows2[i]) {
-          var fields2 = rows2[i].match(/([^\s]+)\s+([^\s]+)\s+([^\s]+)\s+(.+)/);
-          var item = {
-            pid: fields2[1],
-            tt: fields2[2],
-            time: fields2[3],
-            command: fields2[4]
-          };
-          processMap[fields2[1]] = item;
+        if (params) {
+          pids.push(params[1]);
         }
       }
 
-      for (var i = 0; i < rows.length; i++) {
-        try {
-          if (rows[i]) {
-            var fields = rows[i].match(/([^\s]+)\s+([^\s]+)\s+([^\s]+)\s+(.+)/);
-            var item = {
-              pid: fields[1],
-              tt: fields[2],
-              time: fields[3],
-              name: fields[4],
-              // 有可能会出现在 processMap 中找不到对应进程号的情况，待寻找原因
-              command: processMap[fields[1]] && processMap[fields[1]].command || '',
-              isApp: processMap[fields[1]].command.indexOf('.app/Contents') !== -1,
-              icon: '/System/Library/CoreServices/CoreTypes.bundle/Contents/Resources/ExecutableBinaryIcon.icns'
-            };
-
-            if (item.isApp) {
-              // 如果是 app
-              var suffixIndex = processMap[fields[1]].command.indexOf('.app');
-              var appPath = processMap[fields[1]].command.substring(0, suffixIndex + 5);
-
-              item.icon = appPath;
-            }
-
-            items.push(item);
-          }
-        } catch (e) {
-          // 有可能会出现在 processMap 中找不到对应进程号的情况，待寻找原因
-        }
-      }
+      getProcessInfo('\'' + pids.join('\\|') + '\'').then(function(items) {
+        xtpl.renderFile('./processItems.xtpl', {
+          items: items
+        }, function(error, content) {
+          console.log(content);
+        });
+      });
+    });
+  } else {
+    getProcessInfo(params[3]).then(function(items) {
       xtpl.renderFile('./processItems.xtpl', {
         items: items
       }, function(error, content) {
         console.log(content);
       });
     });
-  });
+  }
 };
+
+/**
+ * 获取进程信息
+ * -c command 栏只包含可执行名称(Change the command column output to just contain the executable name, rather than the full command line.)
+ * @param identifier 可以是进程id、进程名称
+ * @returns {Promise}
+ */
+function getProcessInfo(identifier) {
+  return new Promise(function(resolve, reject) {
+    var items = [];
+
+    // 获取参数
+    exec('ps -eo pid -o args | grep -i ' + identifier + ' | grep -v grep', {
+      encoding: originEncoding,
+      maxBuffer: 1024 * 1024
+    }, function(error, stdout, stderr) {
+      if (error) {
+        console.error('应该是没找到进程信息');
+        reject(error);
+      }
+
+      // 生成 args map
+      var argsRows = iconv.decode(new Buffer(stdout, originEncoding), targetEncoding).split(/\n/);
+      var argsMap = {};
+
+      for (var i = 0; i < argsRows.length; i++) {
+        if (argsRows[i]) {
+          var fields = argsRows[i].match(/(\d+)\s+(.+)/);
+          argsMap[fields[1]] = fields[2];
+        }
+      }
+
+      // 获取 command
+      exec('ps -eo pid -o comm | grep -i ' + identifier + ' | grep -v grep', {
+        encoding: originEncoding,
+        maxBuffer: 1024 * 1024
+      }, function(error, stdout, stderr) {
+        if (error) {
+          console.error('应该是没找到进程信息');
+          reject(error);
+        }
+
+        var commandRows = iconv.decode(new Buffer(stdout, originEncoding), targetEncoding).split(/\n/);
+
+        for (var i = 0; i < commandRows.length; i++) {
+          if (commandRows[i]) {
+            var fields = commandRows[i].match(/(\d+)\s+(.+)/);
+
+            var lastIndexOfSlash = fields[2].lastIndexOf('\/');
+            var item = {
+              pid: fields[1],
+              command: fields[2],
+              name: fields[2].substring(lastIndexOfSlash === -1 ? 0 : lastIndexOfSlash + 1, fields[2].length),
+              args: argsMap[fields[1]],
+              isApp: fields[2].indexOf('.app/Contents') !== -1,
+              icon: '/System/Library/CoreServices/CoreTypes.bundle/Contents/Resources/ExecutableBinaryIcon.icns'
+            };
+
+            if (item.isApp) {
+              // 如果是 app
+              item.icon = item.command.substring(0, item.command.indexOf('.app') + 5);
+            }
+
+            items.push(item);
+          }
+        }
+
+        resolve(items);
+      });
+    });
+  });
+}
